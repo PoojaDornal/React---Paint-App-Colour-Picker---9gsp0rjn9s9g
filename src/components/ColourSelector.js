4ec8b186b0afcc7530223c1bca7183e5cd1928db
PoojaDornal <37832052+PoojaDornal@users.noreply.github.com>
2022-04-05 15:11:49 +0530

    Update ColourSelector.js

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const ColourSelector = (props) => {
-  const { config, selectNextBackground } = props
+  const { config, selectNextBackground } = props;
   const { background } = config;
   return (
-    <button className={config.classneme} onClick={() => selectNextBackground({background: background})}>
-      {config.label}
+    <button
+      className={config.classname}
+      onClick={() => selectNextBackground({ background: background })}
+    >
+      {config.lable}
     </button>
-  )
-}
+  );
+};
 export default ColourSelector;
